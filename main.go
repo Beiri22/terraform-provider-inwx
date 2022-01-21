1804db49b60d89d732c08e1490246135695c8bfb
Beiri22 <Beiri22ny@googlemail.com>
2022-01-21 18:45:42 +0100

    chg

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/andrexus/terraform-provider-inwx/inwx"
+	"github.com/beiri22/terraform-provider-inwx/inwx"
 )
 
 func main() {
